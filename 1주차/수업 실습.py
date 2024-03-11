fc60f1755d2a51ac76182a40b368deb6ce971103
[24YG] <[glory0926@tukorea.ac.kr]>
2024-03-11 14:31:51 +0900

    1

---
+++ 1주차/수업 실습.py
@@ -0,0 +1,14 @@
+print("hello world" * 10) # hello world 10번 반복 
+
+a = "tukorea"
+print(a[:3])
+
+print("hello \nworld")
+
+print('%d / %d = %.2f ' % (200,100,2.0)) # %n.mf()에서 n은 n만큼의 자릿수 확보 / m은 소수점 m번째 자리까지 출력
+print("{0:d} {1:d} {2:d}".format(100,200,300)) 
+
+print("뉴\n라인")
+print("tab\t키")
+print("백스페 \b이스")
+print("문자열\"따옴표\"출력")
