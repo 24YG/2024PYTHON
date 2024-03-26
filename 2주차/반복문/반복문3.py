255a51ff25a2ebc9a07943ab246d90f65c688049
[24YG] <[glory0926@tukorea.ac.kr]>
2024-03-26 13:18:55 +0900

    1

---
+++ 2주차/반복문/반복문3.py
@@ -0,0 +1,13 @@
+N = int(input("30이하의 정수를 입력하시오 : "))
+
+c = 1
+
+if c == 1 : 
+    for N in range(N+1,31) :
+         print(N)
+         N += 1
+         
+elif c ==2 :
+    while N <= 30 :
+        print(N)
+        N += 1
