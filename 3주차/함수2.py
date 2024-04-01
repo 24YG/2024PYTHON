4171e7b75e6f4178214cfd87e97028a7f6d24bf2
[24YG] <[glory0926@tukorea.ac.kr]>
2024-04-01 15:05:57 +0900

    1

--- 3주차/함수2.py
+++ 3주차/함수2.py
@@ -1,3 +1,4 @@
+
 def get_power(x,y):
     return x**y
 
@@ -5,10 +6,11 @@ def multiolicationTable(dan) :
     for i in range(1,10) :
         print("%dX%d=%d"%(dan,i,(dan*i)))
         
-def factorial(x):
+def factorial():
     fact=1
     for i in range(1,x+1):
         fact *=i
     return fact
 
 print(factorial(10))
+
